43ee7498681b9042716cc340401dea07fa019a99
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-05-19 07:49:53 +0000

    [Update]model

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -6,4 +6,14 @@ class Shop < ApplicationRecord
 
   has_many :items, dependent: :destroy
 
+  has_one_attched :shop_image
+
+  def get_shop_image(width, height)
+    unless shop_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      shop_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    shop_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
