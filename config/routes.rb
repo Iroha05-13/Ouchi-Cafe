2907beae608f25966967daf329ed95c254e6b0cc
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-05-19 05:47:12 +0000

    [Add]devise:controller,views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :shops
-  devise_for :users
-  devise_for :admins
+  devise_for :shops, controllers: {
+    registrations: "shop/registrations",
+    sessions: "shop/sessions"
+  }
+
+  devise_for :users, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
 end
