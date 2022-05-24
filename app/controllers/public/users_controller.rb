bf6855b3bab1b84d33712e3581ffd84a8fa87c41
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-05-24 16:40:41 +0000

    [Update]public/users

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,21 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def unsubscribe
+  end
+
+  def update
+    user = current_user
+    if user.update(user_params)
+      redirect_to users_path(user.id)
+    else
+      render :edit
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     sessions: "shop/sessions"
   }
 
-  devise_for :users, controllers: {
+  devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
@@ -15,44 +15,44 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :users, except: [:new, :destroy]
-    
+
     resources :shops, only: [:show,:destroy]
-    
+
     root to: 'homes#top'
   end
-  
+
   scope module: :public do
     resources :addresses, except: [:new]
-    
+
     post 'orders/confirm', as: 'confirm'
     get 'orders/complete', as: 'complete'
     resources :orders, except: [:edit, :update, :destroy]
-    
+
     get 'users/unsubscribe', as: 'unsubscribe'
     patch 'users/withdrawal', as: 'withdrawal'
     resource :users, only: [:show, :edit, :update] do
       resources :favorites, only: [:index]
     end
-    
+
     root to: "homes#top"
     get '/about' => "homes#about", as: "about"
-    
+
     delete 'cart_items/destroy_all', as: 'destroy_all'
     resources :cart_items, except: [:show, :new]
-    
+
     resources :shops, only: [:index, :show]
-    
+
     resources :items, only: [:index, :show] do
       resource :favorites, only: [:create, :destroy]
       resources :post_comments, only: [:new, :create, :destroy]
     end
   end
-  
+
   scope module: :shop do
     get 'shop_user/:id' => 'shops#show', as: 'show_shop'
     resources :shops, only: [:edit, :update]
   end
-  
+
   namespace :shop do
     resources :items, except: [:show, :destroy]
     resources :orders, only: [:index, :show, :update]
