57ac82d6d70323634c8a42776cd5e9c5e6403513
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-05-28 08:47:44 +0000

    [Add}gem enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,7 @@ class Order < ApplicationRecord
 
   belongs_to :user
 
+  enum payment: { credit_card: 0, transfer: 1 }
+  enum status: { waiting: 0, paid_up: 1, making: 2, preparing: 3, shipped: 4 }
+
 end
