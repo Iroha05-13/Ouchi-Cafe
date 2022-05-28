0e9bfcf1cb70c718938755066763979ae0082b98
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-05-28 08:38:00 +0000

    [Add}gem enum

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,12 +2,17 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
   end
+  
+  def create
+    @order = Order.new(order_params)
+  end
 
   def index
     @orders = current_user.orders
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def confirm
