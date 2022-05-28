4f75eb792b4abf9d5786bf8ba18e16437b1ca813
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-05-28 07:45:23 +0000

    [Update]public/addresses

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,48 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = current_user.addresses
+  end
+
+  def create
+    address = Address.new(address_params)
+    address.user_id = current_user.id
+    if address.save
+      flash[:notice] = "配送先を追加しました"
+      redirect_to addresses_path
+    else
+      @addresses = current_user.addresses
+      render :index
+    end
   end
 
   def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    address = Address.find(params[:id])
+    if address.update(address_params)
+      flash[:notice] = "配送先を更新しました"
+      redirect_to addresses_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    address = Address.find(params[:id])
+    if address.destroy
+      flash[:notice] = "配送先を削除しました"
+      redirect_to addresses_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
   end
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,8 +1,10 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def index
+    @orders = current_user.orders
   end
 
   def show
