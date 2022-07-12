912626d65aafb88d958b372c5000426a9d499284
Iroha05-13 <100775168+Iroha05-13@users.noreply.github.com>
2022-07-12 03:19:35 +0000

    [Update]public/homes

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
-    @items = Item.order("RANDOM()").limit(1)
-    @shops = Shop.order("RANDOM()").limit(1)
+    @items = Item.order("RAND()").limit(1)
+    @shops = Shop.order("RAND()").limit(1)
   end
 
   def about
